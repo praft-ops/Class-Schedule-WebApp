f4e1bcf534ba359ec29da080f54bf7302f480503
Patrick Raftery <praftery@ivytech.edu>
2022-05-15 17:24:33 -0400

    Cleaning up db.js file

--- db.js
+++ db.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 
-// mongoose.set('useUnifiedTopology', true);
-//const coursedb = mongoose.connect("mongodb://localhost/coursesdb",
-//  {useNewUrlParser: true});
-    module.exports = mongoose;
+module.exports = mongoose;
 
 const studentSchema = new mongoose.Schema({
     username: { type: String, required: true },
@@ -11,7 +8,6 @@ const studentSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     teacher: Boolean,
-    courses: [ Number ]
 });
 
 const teacherSchema = new mongoose.Schema({
@@ -20,18 +16,17 @@ const teacherSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     teacher: Boolean,
-    courses: [ Number ]
 });
 
 const courseSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true },
     courseName: String,
     courseNumber: Number,
     Students: [ String ],
-    Teacher: [ String ]
+    Teacher: String // Only one teacher allowed per course. 
 });
 
+// export defined modules
 module.exports = mongoose
 module.exports = teacherSchema;
 module.exports = studentSchema;
+module.exports = courseSchema;
