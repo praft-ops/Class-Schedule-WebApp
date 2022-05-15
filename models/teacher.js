e7887761fe061045662f1ff67c8f11b47333865d
Patrick Raftery <praftery@ivytech.edu>
2022-05-15 17:23:46 -0400

    Altering Schema

--- models/teacher.js
+++ models/teacher.js
@@ -1,5 +1,6 @@
 // Create a "Teacher" model from the teacherSchema schema
 const mongoose = require('mongoose');
+
 const Teacher = mongoose.model("Teacher",{
     username: { type: String, required: true },
     password: { type: String, required: true },
