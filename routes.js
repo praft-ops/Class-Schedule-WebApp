acf61d207d1e5f150bc5a2f2fb3fac25cf6f12ec
Patrick Raftery <praftery@ivytech.edu>
2022-05-15 17:38:17 -0400

    Added Teacher/register/view for register form

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const jwt = require('jwt-simple');
 const Student = require("./models/student");
+const Teacher = require("./models/teacher");
 const app = express();
 
 // For encoding/decoding JWT
@@ -29,6 +30,32 @@ app.post("/student/register", async (req, res) => {
         }
         else {
             res.sendStatus(201, () => console.log(`${newStudent}`) );
+        }
+    });
+});
+
+//Add a new teacher to the database
+app.post("/teacher/register", async (req, res) => {
+
+    // Validate Form Fields
+    if (!req.body.username || !req.body.password || !req.body.firstName || !req.body.lastName) {
+        res.status(400).json({ error: "Missing username/Password/FirstName/LastName"});
+        return;
+    }
+    // Assign request body variables to newTeacher object
+    const newTeacher = new Teacher({
+        username: req.body.username,
+        password: req.body.password,
+        status: req.body.status
+    });
+
+    // Save newTeacher to database
+    newTeacher.save(function(err) {
+        if (err) {
+            res.status(400).send(err);
+        }
+        else {
+            res.sendStatus(201, () => console.log(`${newTeacher}`) );
         }
     });
 });
